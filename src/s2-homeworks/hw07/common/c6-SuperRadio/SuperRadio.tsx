9dee33a39f1faf7ee02d0a49de6cbb316cabf9f9
dasha7879 <belka7879@gmail.com>
2023-02-21 14:37:23 +0200

    edit name

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      name = {o.id}
+                      name = {o.name}
                       value = {o.id}
                       checked={o.id==value}
                       // name, checked, value делают студенты
